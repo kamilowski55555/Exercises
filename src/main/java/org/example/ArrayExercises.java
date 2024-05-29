5fea3441127e9c84721322b03f12a23a8dbcf02e
Kamil <kamilkot55555@gmail.com>
2024-05-29 08:44:57 +0200

    Some more array exercises

--- src/main/java/org/example/ArrayExercises.java
+++ src/main/java/org/example/ArrayExercises.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class ArrayExercises {
 
+    public int countClumps(int[] nums) {
+        int clumps = 0;
+        int i = 0;
+
+        while (i < nums.length) {
+            int count = 1;
+            while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                count++;
+                i++;
+            }
+            if (count > 1) {
+                clumps++;
+            }
+            i++;
+        }
+
+        return clumps;
+    }
+
     public int[] seriesUp(int n) {
         int length = n * (n + 1) / 2;
         int[] result = new int[length];
